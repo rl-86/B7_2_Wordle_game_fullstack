689ce0a3ff3e36c6846fdbc173631d396a2483a1
rl-86 <rl86@live.se>
2024-04-12 09:37:58 +0200

    Added some color variables

--- frontend/src/components/GuessInput.jsx
+++ frontend/src/components/GuessInput.jsx
@@ -1,8 +1,28 @@
-export default function GuessInput(props) {
+import { useState } from 'react';
+
+export default function GuessInput() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleGuess = (event) => {
+    event.preventDefault();
+    console.log('Guess:', inputValue.toUpperCase());
+    setInputValue('');
+  };
+
   return (
     <div>
-      <input type='text' value={props.guess} onChange={props.onGuessChange} />
-      <button>Guess</button>
+      <form onSubmit={handleGuess}>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleChange}
+          placeholder='Your Guess'
+        />
+        <button type='submit'>Guess</button>
+      </form>
     </div>
   );
 }
--- frontend/src/components/Letter.jsx
+++ frontend/src/components/Letter.jsx
@@ -3,11 +3,44 @@ import { useState } from 'react';
 export default function Letter() {
   const [letterState, setLetterState] = useState('state1');
 
-  const changeLetterState = () => {};
+  const changeLetterState = () => {
+    switch (letterState) {
+      case 'state1':
+        setLetterState('state2');
+        break;
+      case 'state2':
+        setLetterState('state3');
+        break;
+      case 'state3':
+        setLetterState('state4');
+        break;
+      case 'state4':
+        setLetterState('state1');
+        break;
+    }
+  };
+
+  const getFeedbackColor = () => {
+    switch (letterState) {
+      case 'state1':
+        return 'var(--letterDefault-color)';
+      case 'state2':
+        return 'var(--letterIncorrect-color)';
+      case 'state3':
+        return 'var(--letterMisplaced-color)';
+      case 'state4':
+        return 'var(--letterCorrect-color)';
+    }
+  };
 
   return (
     <div>
-      <button>A</button>
+      <button
+        style={{ backgroundColor: getFeedbackColor() }}
+        onClick={changeLetterState}
+      >
+        A
+      </button>
     </div>
   );
 }
